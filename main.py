34b8d377bd2f1b8c0db75f2e9f4738dde6e6c23e
Siddharth <sid28071998@gmail.com>
2022-03-09 18:53:42 +0530

    set screen and paddle

---
+++ main.py
@@ -0,0 +1,23 @@
+from turtle import Screen
+from paddle import Paddle
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_on = True
+while game_on:
+    screen.update()
+screen.exitonclick()
